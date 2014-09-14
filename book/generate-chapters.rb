589572ded7babfee888bad502a7399c5b37071ff
Jason van Zyl <jason@tesla.io>
2014-09-14 15:03:15 -0400

    Update the generation to deal with outputing the pages in a format that Jekyll can consume

--- book/generate-chapters.rb
+++ book/generate-chapters.rb
@@ -17,13 +17,34 @@ Dir.glob("../[0-9][0-9]-*.md") { |file|
 #
 # en/01-preface/01-chapter1.markdown
 #
+# We also want to emit a version of the chapter with Jekyll front-matter that can
+# be integrated into a normal Jekyll site
+#
+bookDirectory = "team/book"
+FileUtils.mkdir_p(bookDirectory)
 chapters.each { |chapter|
   basename = File.basename(chapter, ".md")
-  chapterNumber = basename.match(/([0-9][0-9])-*/)[1]
+  parts = basename.match(/([0-9][0-9])-(.*)/)
+  chapterNumber = parts[1]
+  title = parts[2].capitalize
   chapterDirectory = "en/#{basename}"
+  #
+  # PDF/ebook content   # 
   FileUtils.mkdir_p chapterDirectory
   transformedChapterFile = "#{chapterDirectory}/#{chapterNumber}-chapter1.markdown"
   FileUtils.cp(chapter, transformedChapterFile)
-  puts chapter
+  #
+  # Jekyll content
+  #
+  jekyllPath = "#{bookDirectory}/#{basename}.md"
+  content = File.open(chapter).read
+  File.open(jekyllPath, 'w') { |f| 
+    f.write("---\n")
+    f.write("layout: chapter\n")
+    f.write("title: #{title}\n")
+    f.write("---\n")
+    f.write(content) 
+  }
 }
+
 
