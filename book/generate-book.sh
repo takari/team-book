f4b8794f6c1c80bb5441cc38819b758cafe8d5cc
Manfred Moser <manfred@simpligility.com>
2014-10-07 21:05:43 -0700

    updated build script a bit and added a howto file

--- book/generate-book.sh
+++ book/generate-book.sh
@@ -5,23 +5,25 @@ set -e
 # fail if a function call is missing an argument
 set -u
 
-rm -rf en
+rm -rf team/book
+echo "build - Removed previous build output ---------------------------------------------"
 
 ./generate-chapters.rb
+echo "build - Generated chapters----- ---------------------------------------------------"
 
-echo "build - Starting PDF Generation -----------------------------------------------"
+echo "build - Starting PDF Generation ---------------------------------------------------"
 ./makepdfs.sh en
-echo "build - Finshed PDF Generation - see team-book.en.pdf --------------------------"
+echo "build - Finished PDF Generation - see team/book/team.pdf --------------------------"
 
-echo "build - Starting mobi Generation ----------------------------------------------"
+echo "build - Starting mobi Generation --------------------------------------------------"
 export FORMAT=mobi
 ./makeebooks.rb en
-echo "build - Finished mobi Generation - see team-book.en.mobi -----------------------"
+echo "build - Finished mobi Generation - see team/book/team.mobi ------------------------"
 
-echo "build - Starting epub Generation ----------------------------------------------"
+echo "build - Starting epub Generation --------------------------------------------------"
 export FORMAT=epub
 ./makeebooks.rb en
-echo "build - Finished epub Generation - see team-book.en.epub -----------------------"
+echo "build - Finished epub Generation - see team/book/team.epub ------------------------"
 
 # We dont' want to keep this around
 rm team-book.en.html
@@ -29,4 +31,5 @@ rm team-book.en.html
 mv team-book.en.epub team/book/team.epub
 mv team-book.en.mobi team/book/team.mobi
 mv team-book.en.pdf team/book/team.pdf
-
+echo "build - Completed - see content of team/book: --------------------------------------"
+ls -1 team/book
